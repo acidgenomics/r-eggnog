23cc7c8c1ec7e788eb399e1d53fe4eff8161a626
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-21 15:16:56 -0500

    Work toward improved release support

--- R/package.R
+++ R/package.R
@@ -7,7 +7,8 @@
 #'
 #' @importFrom basejump SimpleList cacheURL import metadata<- packageName
 #'   packageVersion pasteURL rbind showHeader showSlotInfo
-#' @importFrom goalie assert hasInternet isAFile isAURL isString validate
+#' @importFrom goalie assert hasInternet isAFile isAURL isString isSubset
+#'   validate
 #' @importFrom methods as new setClass setValidity show
 #' @importFrom stringr str_match str_subset
 "_PACKAGE"
