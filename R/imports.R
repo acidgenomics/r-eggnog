49ba87ffc3c822d20b80ad760352f1a60a6569bb
Michael Steinbaugh <mike@steinbaugh.com>
2022-06-07 16:11:17 -0400

    Rework NAMESPACE

---
+++ R/imports.R
@@ -0,0 +1,36 @@
+## S4 classes ==================================================================
+
+#' @importClassesFrom IRanges SimpleDFrameList
+NULL
+
+
+
+## S4 generics and methods =====================================================
+
+#' @importFrom BiocGenerics rbind
+#' @importFrom S4Vectors metadata<-
+#' @importFrom methods show
+#' @importFrom pipette import
+#'
+#' @importMethodsFrom pipette import
+NULL
+
+
+
+## Standard functions ==========================================================
+
+#' @importFrom AcidBase pasteURL showHeader showSlotInfo
+#' @importFrom S4Vectors SimpleList
+#' @importFrom goalie assert hasInternet isAFile isAURL isString isSubset
+#' validate
+#' @importFrom methods as new setClass setValidity
+#' @importFrom pipette cacheURL
+#' @importFrom utils packageName packageVersion
+NULL
+
+
+
+## FIXME Need to rework these
+
+#' @importFrom stringr str_match str_subset
+NULL
