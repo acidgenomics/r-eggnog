f01bb719ecd70f5353454b2a597f97eeaf26c925
Michael Steinbaugh <mike@steinbaugh.com>
2022-06-08 08:22:06 -0400

    Resave class as factor

--- inst/extdata/cog-functional-categories.R
+++ inst/extdata/cog-functional-categories.R
@@ -5,4 +5,5 @@ suppressPackageStartupMessages({
 ## nolint end
 object <- import("cog-functional-categories.csv")
 object <- as(object, "DataFrame")
+object[["class"]] <- as.factor(object[["class"]])
 saveRDS(object, "cog-functional-categories.rds")
