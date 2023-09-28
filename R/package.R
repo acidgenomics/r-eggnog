2654f7a435a0d4c53469b8ec1ab6e742f0852b9c
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-28 09:24:09 -0400

    Move imports to package.R

--- R/package.R
+++ R/package.R
@@ -5,3 +5,35 @@
 #' @aliases NULL
 #' @keywords internal
 "_PACKAGE"
+
+
+
+## S4 classes ==================================================================
+
+#' @importClassesFrom IRanges SimpleDFrameList
+NULL
+
+
+
+## S4 generics and methods =====================================================
+
+#' @importFrom BiocGenerics do.call rbind
+#' @importFrom S4Vectors metadata<-
+#' @importFrom methods show
+#' @importFrom pipette import
+#'
+#' @importMethodsFrom pipette import
+NULL
+
+
+
+## Standard functions ==========================================================
+
+#' @importFrom AcidBase pasteUrl showHeader showSlotInfo
+#' @importFrom S4Vectors SimpleList
+#' @importFrom goalie assert hasInternet isAFile isAUrl isString isSubset
+#' validate
+#' @importFrom methods as new setClass setValidity
+#' @importFrom pipette cacheUrl
+#' @importFrom utils packageName packageVersion
+NULL
